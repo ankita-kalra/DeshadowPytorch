37aeffde05d11415ac109059200fc0c78f9b2ddc
Ankita Kalra <ankita.kalra30@gmail.com>
2018-08-02 15:40:48 -0700

    dataloader updated

--- shadow_dataset_loader.py
+++ shadow_dataset_loader.py
@@ -6,13 +6,55 @@ import torchvision.transforms as transforms
 from PIL import Image
 
 
-def make_dataset(img_path, label_path):
-    dataset = []
-    for img in glob.glob(os.path.join(img_path, '*.jpg')):
-        basename = os.path.basename(img)
-        image = os.path.join(img_path, basename)
-        label = os.path.join(label_path, basename)
-        dataset.append([image, label])
-    return dataset
+def make_shadow_pair_dataset(dir):
+    images = []
+    srimages = []
+    for root, _, fnames in sorted(os.walk(dir)):
+        for fname in fnames:
+            if is_image_file(fname):
+                path = os.path.join(root, fname)
+                srimages = path.replace("images","srimages").replace("jpg","png")
+                images.append(path)
+                srimages.append(srimpath)
+    return images, srimages
 
-dataset=make_dataset()
+ #cant be done during training, as tensor is the input   
+
+def rgb_loader(path):
+    return Image.open(path).convert('RGB')
+
+def gray_loader(path):
+    return Image.open(path).convert('L')
+
+class CustomShadowPairDataset(data.Dataset):
+
+    def __init__(self, root, transform=None, target_transform=None,
+                 rgb_loader=rgb_loader, gray_loader=gray_loader):
+        imgs, srimgs = make_double_dataset(root)
+        if len(imgs) == 0:
+            raise(RuntimeError("Found 0 images in subfolders of: " + root + "\n"
+                               "Supported image extensions are: " + ",".join(IMG_EXTENSIONS)))
+
+        self.root = root
+        self.imgs = imgs
+        self.srimgs = srimgs
+        self.transform = transform
+        self.target_transform = target_transform
+        self.rgb_loader = rgb_loader
+        self.gray_loader = gray_loader
+
+    def __getitem__(self, index):
+        imgpath = self.imgs[index]
+        srimgs = self.srimgs[index]
+
+        img = self.rgb_loader(imgpath)
+        srimg = self.gray_loader(srimg)
+
+        if self.transform is not None:
+            img = self.transform(img)
+            srimg = self.transform(srimg)
+
+        return img, srimg
+
+    def __len__(self):
+        return len(self.imgs)
